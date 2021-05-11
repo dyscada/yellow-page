47e255e1d3b74c71413f8d9dd616b2699d2d134b
dyscada <dyscada@gmail.com>
2021-05-11 19:31:47 +0900

    Change Source

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener("scroll", () => {
 
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener(`click`, (event) => {
+navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
@@ -29,7 +29,7 @@ navbarToggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
-//Handle click on "contact me" button on home
+//Handle click on "contact us" button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
@@ -42,7 +42,7 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-//Show "arrow up " button when scrolling down
+//Show "arrow-up" button when scolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -52,7 +52,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-//Handle click on the "arrow up" button
+//Handle click on the "arrow-up" button
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
@@ -69,15 +69,14 @@ workBtnContainer.addEventListener("click", (e) => {
 
   //Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
@@ -88,17 +87,11 @@ workBtnContainer.addEventListener("click", (e) => {
   }, 300);
 });
 
-// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
-// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
-const sectionIds = [
-  "#home",
-  "#about",
-  "#skills",
-  "#work",
-  "#testimonials",
-  "#contact",
-];
+//1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다.
+//2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = ["#home", "#about", "#work", "#contact"];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[data-link="${id}"]`)
@@ -106,6 +99,7 @@ const navItems = sectionIds.map((id) =>
 
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
+
 function selectNavItem(selected) {
   selectedNavItem.classList.remove("active");
   selectedNavItem = selected;
@@ -114,8 +108,8 @@ function selectNavItem(selected) {
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: `smooth` });
-  selectedNavItem(navItems[sectionIds.indexOf(selector)]);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 const observerOptions = {
@@ -128,7 +122,8 @@ const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting && entry.intersectionRatio > 0) {
       const index = sectionIds.indexOf(`#${entry.target.id}`);
-      // 스크롤링이 아래로 되어서 페이지가 올라옴
+
+      //스크롤링이 아래로 되어서 페이지가 올라옴
       if (entry.boundingClientRect.y < 0) {
         selectedNavIndex = index + 1;
       } else {
@@ -141,12 +136,11 @@ const observerCallback = (entries, observer) => {
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
 
-window.addEventListener("scroll", () => {
-  //scroll과 wheel의 차이 wheel은 scroll bar로 scroll할때 반응을 하지 않음!!!
+window.addEventListener("wheel", () => {
   if (window.scrollY === 0) {
     selectedNavIndex = 0;
   } else if (
-    Math.round(window.scrollY + window.innerHeight) >=
+    window.scrollY + window.innerHeight ===
     document.body.clientHeight
   ) {
     selectedNavIndex = navItems.length - 1;
--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-let scene, camera, rendered, cube;
+let scene, camera, renderer, cube;
 
 function parentWidth(elem) {
   return elem.parentElement.clientWidth;
@@ -16,28 +16,37 @@ function parentHeight(elem) {
   return elem.parentElement.clientHeight;
 }
 
-function init3D(){
+function init3D() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
 
-  camera = new THREE.PerspectiveCamera(75, parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube")), 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(
+    75,
+    parentWidth(document.getElementById("3Dcube")) /
+      parentHeight(document.getElementById("3Dcube")),
+    0.1,
+    1000
+  );
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+  renderer.setSize(
+    parentWidth(document.getElementById("3Dcube")),
+    parentHeight(document.getElementById("3Dcube"))
+  );
 
-  document.getElementById('3Dcube').appendChild(renderer.domElement);
+  document.getElementById("3Dcube").appendChild(renderer.domElement);
 
   // Create a geometry
   const geometry = new THREE.BoxGeometry(5, 1, 4);
 
   // Materials of each face
   var cubeMaterials = [
-    new THREE.MeshBasicMaterial({color:0x03045e}),
-    new THREE.MeshBasicMaterial({color:0x023e8a}),
-    new THREE.MeshBasicMaterial({color:0x0077b6}),
-    new THREE.MeshBasicMaterial({color:0x03045e}),
-    new THREE.MeshBasicMaterial({color:0x023e8a}),
-    new THREE.MeshBasicMaterial({color:0x0077b6}),
+    new THREE.MeshBasicMaterial({ color: 0x03045e }),
+    new THREE.MeshBasicMaterial({ color: 0x023e8a }),
+    new THREE.MeshBasicMaterial({ color: 0x0077b6 }),
+    new THREE.MeshBasicMaterial({ color: 0x03045e }),
+    new THREE.MeshBasicMaterial({ color: 0x023e8a }),
+    new THREE.MeshBasicMaterial({ color: 0x0077b6 }),
   ];
 
   const material = new THREE.MeshFaceMaterial(cubeMaterials);
@@ -49,65 +58,98 @@ function init3D(){
 }
 
 // Resize the 3D object when the browser window changes size
-function onWindowResize(){
-  camera.aspect = parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube"));
+function onWindowResize() {
+  camera.aspect =
+    parentWidth(document.getElementById("3Dcube")) /
+    parentHeight(document.getElementById("3Dcube"));
   //camera.aspect = window.innerWidth /  window.innerHeight;
   camera.updateProjectionMatrix();
   //renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
-
+  renderer.setSize(
+    parentWidth(document.getElementById("3Dcube")),
+    parentHeight(document.getElementById("3Dcube"))
+  );
 }
 
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 
 // Create the 3D representation
 init3D();
 
 // Create events for the sensor readings
 if (!!window.EventSource) {
-  var source = new EventSource('/events');
+  var source = new EventSource("/events");
 
-  source.addEventListener('open', function(e) {
-    console.log("Events Connected");
-  }, false);
+  source.addEventListener(
+    "open",
+    function (e) {
+      console.log("Events Connected");
+    },
+    false
+  );
 
-  source.addEventListener('error', function(e) {
-    if (e.target.readyState != EventSource.OPEN) {
-      console.log("Events Disconnected");
-    }
-  }, false);
+  source.addEventListener(
+    "error",
+    function (e) {
+      if (e.target.readyState != EventSource.OPEN) {
+        console.log("Events Disconnected");
+      }
+    },
+    false
+  );
 
-  source.addEventListener('gyro_readings', function(e) {
-    //console.log("gyro_readings", e.data);
-    var obj = JSON.parse(e.data);
-    document.getElementById("gyroX").innerHTML = obj.gyroX;
-    document.getElementById("gyroY").innerHTML = obj.gyroY;
-    document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+  source.addEventListener(
+    "gyro_readings",
+    function (e) {
+      //console.log("gyro_readings", e.data);
+      var obj = JSON.parse(e.data);
+      document.getElementById("gyroX").innerHTML = obj.gyroX;
+      document.getElementById("gyroY").innerHTML = obj.gyroY;
+      document.getElementById("gyroZ").innerHTML = obj.gyroZ;
 
-    // Change cube rotation after receiving the readinds
-    cube.rotation.x = obj.gyroY;
-    cube.rotation.z = obj.gyroX;
-    cube.rotation.y = obj.gyroZ;
-    renderer.render(scene, camera);
-  }, false);
+      // Change cube rotation after receiving the readinds
+      cube.rotation.x = obj.gyroY;
+      cube.rotation.z = obj.gyroX;
+      cube.rotation.y = obj.gyroZ;
+      renderer.render(scene, camera);
+    },
+    false
+  );
 
-  source.addEventListener('temperature_reading', function(e) {
-    console.log("temperature_reading", e.data);
-    document.getElementById("temp").innerHTML = e.data;
-  }, false);
+  source.addEventListener(
+    "temperature_reading",
+    function (e) {
+      console.log("temperature_reading", e.data);
+      document.getElementById("temp").innerHTML = e.data;
+    },
+    false
+  );
 
-  source.addEventListener('accelerometer_readings', function(e) {
-    console.log("accelerometer_readings", e.data);
-    var obj = JSON.parse(e.data);
-    document.getElementById("accX").innerHTML = obj.accX;
-    document.getElementById("accY").innerHTML = obj.accY;
-    document.getElementById("accZ").innerHTML = obj.accZ;
-  }, false);
+  source.addEventListener(
+    "accelerometer_readings",
+    function (e) {
+      console.log("accelerometer_readings", e.data);
+      var obj = JSON.parse(e.data);
+      document.getElementById("accX").innerHTML = obj.accX;
+      document.getElementById("accY").innerHTML = obj.accY;
+      document.getElementById("accZ").innerHTML = obj.accZ;
+    },
+    false
+  );
+
+  source.addEventListener(
+    "ntp_reading",
+    function (e) {
+      console.log("ntp_reading", e.data);
+      document.getElementById("yymmdd").innerHTML = e.data;
+    },
+    false
+  );
 }
 
-function resetPosition(element){
+function resetPosition(element) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/"+element.id, true);
+  xhr.open("GET", "/" + element.id, true);
   console.log(element.id);
   xhr.send();
 }
